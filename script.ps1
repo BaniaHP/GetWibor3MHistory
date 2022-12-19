c5683ab7b1ce78aaf45be7ba72c9f1f045205ea1
BaniaHP <pawel.bankowski@gmail.com>
2022-12-19 15:48:50 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
-#Source of dates
-#in my example it has only one column "Date"
-# Dates must be in format: yyyy-MM-dd
+# Source of dates
+# In my example it has only one column "Date"
+# Date must be in format: yyyy-MM-dd
 
 $a = Import-Csv "dates.csv"
 
@@ -8,17 +8,18 @@ $report = "Date;Value"
 
 foreach($b in $a)
 {
-  #create URI using date from file
+  # create URI using date from file
   $uri = "https://wyborcza.biz/Waluty/0,112676,,,,WIBOR3M,"+$b.Date+".html"
   
-  #getting web page data
+  # getting web page data
   $r = Invoke-WebRequest -Uri $uri -WebSession $session
   
   # finding value
   $x = ($r.parsedhtml.getelementsbytagname("TR") |% { ( $_.children[1] | ?{ $_.tagName -eq "td"} | % innerText ) } | Select-Object -First 1)
 
-  #preparing report [output data]
+  # preparing report [output data]
   $report = $report + "`n"
   $report = $report + $b.Date +";"+$x
 }
+# final data
 $report
